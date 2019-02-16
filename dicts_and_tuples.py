c13ddaa95002c48870cb58480d4741ca58c16ad0
mallph <39816513+mallph@users.noreply.github.com>
2019-02-16 11:51:57 -0800

    Update dicts_and_tuples.py

--- dicts_and_tuples.py
+++ dicts_and_tuples.py
@@ -2,18 +2,24 @@
 # Assignment 5
 # python 3
 
-fname = 'mbox.txt'
-# come back to update with user input with try/except
-fhand = open(fname)
-d = dict()
+import sys
+fname = input('Enter mbox or mbox-variant filename: ') 
+
+try:
+    fhand = open(fname)
+except:
+    print('Could not find specified file. Please ensure filename is correct and/or the file is in the directory.')
+    sys.exit(0)
+    
+d = dict()  # initalize empty dictionary for email addresses before iterating
 for line in fhand:
-	if not line.startswith('From '):continue
-	words = line.split()
-	d[words[1]] = d.get(words[1],0) + 1
+    if not line.startswith('From '): continue  # included space to ignore possible 'From:' lines in file
+    words = line.split()
+    d[words[1]] = d.get(words[1], 0) + 1
 
 tlist = list()
 for key, val in d.items():
-	tlist.append((val, key))
+    tlist.append((val, key))  # extra set of parentheses to create tuple; value before key for sorting purposes 
 
 tlist.sort(reverse=True)
 most = tlist[0]
